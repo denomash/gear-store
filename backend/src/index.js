ab1a946bc48030b596b7901bba480d0a125257b4
denomash <dennismacharia4747@gmail.com>
2020-05-08 14:25:45 -0400

    setup graphql

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+import dotenv from "dotenv";
+
+import createServer from "./createServer";
+import db from "./db";
+
+dotenv.config({ path: "../variables.env" });
+
+const server = createServer();
+
+// TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  (deets) => {
+    console.log(`server is now running on port http:/localhost:${deets.port}`);
+  }
+);
