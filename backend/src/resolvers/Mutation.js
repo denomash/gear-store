06f1cdab8fa0f4e2ca25a7b1402fb28749b0336d
denomash <dennismacharia4747@gmail.com>
2020-05-12 16:17:29 -0400

    create items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
 
-module.exports = Mutations;
+    return item;
+  },
+};
+
+export default Mutations;
